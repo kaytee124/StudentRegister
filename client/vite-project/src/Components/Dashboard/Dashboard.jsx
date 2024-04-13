9cb59fcd6fd3cb72f48823fc492e50680116267c
kaytee124 <ktproductions124@gmail.com>
2024-04-13 00:12:03 +0000

    new message

---
+++ client/vite-project/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React , {useState}from 'react'
+import './Dashboard.css'
+import Sidebar from './SideBar Section/Sidebar'
+import Content from './Content/Content'
+import { Link, useNavigate} from 'react-router-dom'
+import Profile from './Profile/Profile'
+
+const Dashboard =() => {
+  return (
+    <div className='dashboard'>
+      <Sidebar/>
+      <div className='dashboard--content'>
+        <Content/>
+        <Profile/>
+      </div>
+
+    </div>
+  )
+}
+
+export default Dashboard
